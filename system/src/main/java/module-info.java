40b3b38a08d7035ce917cd583053b3699f24bee5
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-09-20 16:56:22 -0300

    Update module-info.java

--- system/src/main/java/module-info.java
+++ system/src/main/java/module-info.java
@@ -3,4 +3,9 @@ module conta.system {
     requires javax.inject;
     requires spring.tx;
     requires lombok;
+
+    //Exports
+    exports conta.sistema.usecase.impl;
+    exports conta.sistema.dominio.service;
+    exports conta.adapter;
 }
