8d20d76980e89971d1d83c7687baa9fb9ebbc7fd
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-09-20 18:10:57 -0300

    Update module-info.java

--- system/src/main/java/module-info.java
+++ system/src/main/java/module-info.java
@@ -5,7 +5,9 @@ module conta.system {
     requires lombok;
 
     //Exports
-    exports conta.sistema.usecase.impl;
+    exports conta.sistema.usecase.port;
+    exports conta.sistema.dominio.model;
     exports conta.sistema.dominio.service;
+    exports conta.sistema.usecase.impl;
     exports conta.adapter;
 }
