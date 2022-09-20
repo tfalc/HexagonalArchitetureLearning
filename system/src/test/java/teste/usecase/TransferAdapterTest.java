8db7ad9181845a3af185490493be7bf82b780f5b
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-09-20 17:32:29 -0300

    Create TransferAdapterTest.java

---
+++ system/src/test/java/teste/usecase/TransferAdapterTest.java
@@ -0,0 +1,127 @@
+package teste.usecase;
+
+import conta.sistema.dominio.model.NegocioException;
+import conta.sistema.usecase.port.TransferPortUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Teste de caso de uso - Serviço de Transferência")
+@ContextConfiguration(classes = FirstBuild.class)
+@ExtendWith(SpringExtension.class)
+public class TransferAdapterTest {
+
+    Integer contaCredito = 10;
+    Integer contaDebito = 20;
+    Integer contaInexistente = 30;
+    BigDecimal cem = new BigDecimal(100);
+    BigDecimal cinquenta = new BigDecimal(50);
+
+    @Inject
+    TransferPortUseCase transferPortUseCase;
+
+    @Test
+    @DisplayName("Pesquisa conta com número nulo")
+    void PesquisaContaNull(){
+        System.out.println("=== Testando pesquisa de conta com número nulo ===");
+        try{
+            var conta = transferPortUseCase.getConta(null);
+            assertTrue(conta == null, "Conta deve ser nula.");
+        }catch (NegocioException e){
+            fail("Deve carregar uma conta nula.");
+        }
+    }
+
+    @Test
+    @DisplayName("Pesquisa conta com número inexistente")
+    void pesquisaContaNumeroInexistente(){
+        System.out.println("=== Testando pesquisa de conta com número inexistente ===");
+        try{
+            var conta = transferPortUseCase.getConta(contaInexistente);
+            assertTrue(conta == null, "Conta inexistente");
+        }catch (NegocioException e){
+            fail("Deve carregar uma conta nula.");
+        }
+    }
+
+    @Test
+    @DisplayName("Pesquisa conta com número existente")
+    void pesquisaContaNumeroExistente(){
+        System.out.println("=== Testando pesquisa de conta com número existente ===");
+        try{
+            var conta = transferPortUseCase.getConta(contaCredito);
+            assertTrue(conta != null, "Conta deve ser nula.");
+            System.out.println(conta);
+        }catch (NegocioException e){
+            fail("Conta existente carregada.");
+        }
+    }
+
+    @Test
+    @DisplayName("Conta débito obrigatória")
+    void contadebitoObrigatoria(){
+        try{
+            transferPortUseCase.transfer(null, contaCredito, cinquenta);
+            fail("Conta de débito é obrigatório.");
+        }catch (NegocioException e){
+            assertEquals(e.getMessage(), "Conta débito é obrigatório.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Conta crédito obrigatória")
+    void contaCreditoObrigatoria(){
+        try{
+            transferPortUseCase.transfer(contaDebito, null, cinquenta);
+            fail("Conta de crédito é obrigatório.");
+        }catch (NegocioException e){
+            assertEquals(e.getMessage(), "Conta crédito é obrigatório.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Valor obrigatório")
+    void valorObrigatorio(){
+        try{
+            transferPortUseCase.transfer(contaDebito, contaCredito, null);
+            fail("Valor é obrigatório.");
+        }catch (NegocioException e){
+            assertEquals(e.getMessage(), "Valor é obrigatório.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Conta débito inexistente")
+    void contaDebitoInexistente(){
+        try{
+            transferPortUseCase.transfer(contaInexistente, contaCredito, cinquenta);
+            fail("Conta débito inexistente.");
+        }catch (NegocioException e){
+            assertEquals(e.getMessage(), "Conta débito é inexistente.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Conta crédito inexistente")
+    void contaCrebitoInexistente(){
+        try{
+            transferPortUseCase.transfer(contaDebito, contaInexistente, cinquenta);
+            fail("Conta crédito inexistente.");
+        }catch (NegocioException e){
+            assertEquals(e.getMessage(), "Conta crédito é inexistente.");
+            System.out.println(e.getMessage());
+        }
+    }
+}
