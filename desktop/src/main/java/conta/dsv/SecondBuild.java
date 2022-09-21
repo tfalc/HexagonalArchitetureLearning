c0a61be6daf999431672df8d23b7e9db9608c6b3
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-09-21 12:04:05 -0300

    Create SecondBuild.java

---
+++ desktop/src/main/java/conta/dsv/SecondBuild.java
@@ -0,0 +1,17 @@
+package conta.dsv;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        //Front-end adapters
+        "conta.dsv",
+        "conta.tela",
+        //System objects
+        "conta.sistema",
+        //Fake adapters
+        "conta.adapter"
+})
+public class SecondBuild {
+}
