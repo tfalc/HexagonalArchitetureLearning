79b253382be9cea563bff9934b6b0b5f7238dcb8
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2022-09-21 11:36:03 -0300

    Update module-info.java

--- system/src/main/java/module-info.java
+++ system/src/main/java/module-info.java
@@ -4,10 +4,21 @@ module conta.system {
     requires spring.tx;
     requires lombok;
 
-    //Exports
+    //Exports entry ports
     exports conta.sistema.usecase.port;
+    exports conta.sistema.usecase.impl;
+
+    //Exports business system
     exports conta.sistema.dominio.model;
     exports conta.sistema.dominio.service;
-    exports conta.sistema.usecase.impl;
+
+    //exports out ports
+    exports conta.sistema.port;
     exports conta.adapter;
+
+    //Open Spring Reflection
+    opens conta.sistema.usecase.port;
+    opens conta.sistema.usecase.impl;
+    opens conta.sistema.dominio.service;
+    opens conta.adapter;
 }
